6f5de26e5e652744816ed465fe87f498e9a411db
Arklim <61851015+akella44@users.noreply.github.com>
2023-12-26 22:01:41 +0300

    Init commit

---
+++ src/main/java/project/bankapp/BankAppApplication.java
@@ -0,0 +1,12 @@
+package project.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/bankapp/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
